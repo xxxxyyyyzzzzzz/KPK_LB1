eb98ba11b55761abf8e4f73f1ea56d3c94109e51
xxxxyyyyzzzzzz <87224453+xxxxyyyyzzzzzz@users.noreply.github.com>
2026-06-14 19:19:43 +0000

    fix: add styles.css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
+import "./styles.css";
 
 const router = getRouter();
 
