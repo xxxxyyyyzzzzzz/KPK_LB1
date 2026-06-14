0ebd36a1eca0ded49f9eabdc101285ef0e2b036a
xxxxyyyyzzzzzz <87224453+xxxxyyyyzzzzzz@users.noreply.github.com>
2026-06-14 18:12:13 +0000

    debug: remove styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
-import "./styles.css";
 
 const router = getRouter();
 
