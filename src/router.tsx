6e3310baeb02a0ae0a410614d10f058c56698ca7
xxxxyyyyzzzzzz <87224453+xxxxyyyyzzzzzz@users.noreply.github.com>
2026-06-14 18:15:50 +0000

    debug: remove scrollRestoration

--- src/router.tsx
+++ src/router.tsx
@@ -4,13 +4,10 @@ import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
-
   const router = createRouter({
     routeTree,
     context: { queryClient },
-    scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
-
   return router;
 };
